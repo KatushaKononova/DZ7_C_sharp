251f10699fa5d24a1d1e3975380c96ff254efd02
KatushaKononova56 <83833827+KatushaKononova56@users.noreply.github.com>
2022-10-27 20:17:52 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.WriteLine("Введите 2 целочисленных числа");
+string num1 = Console.ReadLine();
+string num2 = Console.ReadLine();
+if(Int32.Parse(num1)>Int32.Parse(num2)){
+    Console.WriteLine("Большее число: "+ num1 + ", меньшее: " + num2);
+}
+else
+{
+    Console.WriteLine("Большее число: "+ num2 + ", меньшее: " + num1);
+}
